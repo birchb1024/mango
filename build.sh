ca6c509ca70d63bcc892d8e6943658a44fff286d
Peter William Birch <birchb1024@gmail.com>
2020-06-07 23:55:40 +1000

    Buildy McBuildface

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ set -x
 version=$(git describe --abbrev)
 
 go build -o mango -ldflags "-X github.com/birchb1024/mango.Version=${version}" main.go
+strip mango
 GOOS=windows GOARCH=amd64 go build -o mango.exe -ldflags "-X github.com/birchb1024/mango.Version=${version}" main.go
 GOOS=darwin GOARCH=amd64 go build -o mango_mac -ldflags "-X github.com/birchb1024/mango.Version=${version}" main.go
 
 mkdir -p pkg
-tar zcvf pkg/mango-${version}.tgz ./mango ./mango.exe
+tar zcvf pkg/mango-"${version}".tgz ./mango ./mango.exe ./mango_mac ./README.md
